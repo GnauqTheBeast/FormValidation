c83ad0cbb18a6045147af4fcfb4103c5b18cf040
GnauqTheBeast <quangnguyenngoc314@gmail.com>
2023-09-27 17:10:40 +0700

    update

--- myjs.js
+++ myjs.js
@@ -3,9 +3,22 @@ const $$ = document.querySelectorAll.bind(document);
 
 const Validator = function(options) {
 
+  var selectorRules = {};
+
   var validate = function(inputElement, rule) {
     var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
-    var errorMessage = rule.test(inputElement.value);
+    var errorMessage;
+
+    //Get every rule of selector
+    var rules = selectorRules[rule.selector];
+
+    //Loop through every rule, if has mistake, break out loop
+    for(var i = 0; i < rules.length; i++) {
+      errorMessage = rules[i](inputElement.value);
+      if(errorMessage)
+        break;
+    }
+
     if(errorMessage) {
       errorElement.innerHTML = errorMessage; 
       inputElement.parentElement.classList.add('invalid');
@@ -15,12 +28,35 @@ const Validator = function(options) {
       inputElement.parentElement.classList.remove('invalid');
     }
   }
-
-  var formElement = $(`#${options.form}`);
+  
+  var formElement = $(`${options.form}`);
 
   if(formElement) {
+    formElement.onsubmit = function(e) {
+      //prevent Submit 
+      e.preventDefault();
+
+      //When submit, loop every rule to check form submit
+      options.rules.forEach(function(rule) {
+        var inputElement = formElement.querySelector(rule.selector);
+        validate(inputElement, rule);
+      });
+
+    }
+
+    //Loop through every rule
     options.rules.forEach(function(rule) {
+      //Save every rule for a single input
+      if(Array.isArray(selectorRules[rule.selector])) {
+        selectorRules[rule.selector].push(rule.test);
+      }
+      else {
+        selectorRules[rule.selector] = [rule.test];
+      }
+
       var inputElement = formElement.querySelector(rule.selector);
+
+      //HTML DOM EVENT
       if(inputElement) {
         inputElement.onblur = function() {
           validate(inputElement, rule);
@@ -33,7 +69,7 @@ const Validator = function(options) {
       }
     });
   }
-  
+
 }
 
 Validator.isRequired = function(selector) {
